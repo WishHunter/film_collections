fdae007d8a2bedbacc90386aa30a59ee7df166bf
d.molkov <d.molkov@fbs.com>
2023-01-17 15:50:15 +0100

    init project

---
+++ film_collections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  film_collections
+//
+//  Created by Denis Molkov on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
